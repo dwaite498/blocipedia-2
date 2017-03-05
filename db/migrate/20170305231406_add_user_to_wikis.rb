a1bbc7160094128ef9cc01c9913eb23c626a5105
David Waite <dwaite498@gmail.com>
2017-03-05 23:17:09 +0000

    add user to wiki index and add private boolean to wiki, install pundit gem. Pundit still needs to be integrated.

---
+++ db/migrate/20170305231406_add_user_to_wikis.rb
@@ -0,0 +1,5 @@
+class AddUserToWikis < ActiveRecord::Migration
+  def change
+    add_reference :wikis, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303181012) do
+ActiveRecord::Schema.define(version: 20170305231406) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 20170303181012) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "private"
+    t.integer  "user_id"
   end
+
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
 end
