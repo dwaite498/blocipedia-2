28f8d130d5a651abd53c666c8271cec2b3a227e8
David Waite <dwaite498@gmail.com>
2017-03-07 15:04:15 +0000

    make users own wikis and wikis have users. create private wikis.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,5 @@
 class Wiki < ActiveRecord::Base
+  belongs_to :user
   validates :title, length: { minimum: 4 }
   validates :body, length: { minimum: 20 }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,39 @@
 require 'random_data'
 
-# create wikis
+
+User.create!(
+    email: "Test@email.com",
+    password: "password",
+    role: 'user'
+    )
+
 10.times do
-    Wiki.create!(
-        title: RandomData.random_name,
-        body: RandomData.random_paragraph
+    User.create!(
+    email: RandomData.random_email,
+    password: 'password'
     )
 end
 
 User.create!(
-    email: "Test@email.com",
-    password: "password"
+    email: 'dhammer@blocipedia.com',
+    password: 'password',
+    role: 'admin'
     )
+users = User.all
+
+# create wikis
+10.times do
+    Wiki.create!(
+        title: RandomData.random_name,
+        body: RandomData.random_paragraph,
+        user: users.sample,
+        private: false
+    )
+end
+
 
 wikis = Wiki.all
 puts "seed finished"
 puts "#{User.first.email} user created"
 puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"
