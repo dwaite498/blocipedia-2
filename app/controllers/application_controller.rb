68da626e9d8afecc017c11d303011a3ba46ea4a4
David Waite <dwaite498@gmail.com>
2017-03-09 15:51:48 +0000

    WIP authorization wiki policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   include Pundit
   protect_from_forgery with: :exception
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,4 +1,18 @@
 class WikiPolicy < ApplicationPolicy
+  attr_reader :user, :wiki
+  
+  def initialize(user, wiki)
+    @user = user
+    @wikis = wiki
+  end
+  
+  def index?
+    current_user.admin
+  end
+  
+  def update?
+    user.admin? or wiki.private
+  end
   class Scope < Scope
     def resolve
       scope
