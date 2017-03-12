c8a1d345c1de18ab225d633236ae0a30cb4031fd
David Waite <dwaite498@gmail.com>
2017-03-12 17:40:51 +0000

    implement stripe integration and create charges controller

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+ # Store the environment variables on the Rails.configuration object
+Rails.configuration.stripe = {
+    :publishable_key => Rails.application.secrets.stripe_publishable_key,
+    :secret_key      => Rails.application.secrets.stripe_secret_key
+}
+
+Stripe.api_key = Rails.application.secrets.stripe_secret_key
